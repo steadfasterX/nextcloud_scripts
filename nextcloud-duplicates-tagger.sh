53c33e2755c6716fd13695d43c30ec948ea7f4d0
steadfasterX <steadfasterX@users.noreply.github.com>
2024-05-01 16:44:10 +0200

    dup-tagger: greatly enhanced
    - optimized output
    - added new way for deleting massive loads of files in one run by introducing the new {tagName}_delme tag which gets NOT set on the first dup but on any following
    - new --help-tags parameter which explains how to proceed on the resulting tagged files (also shown at the end of each run automatically - not using quiet mode)

--- nextcloud-duplicates-tagger.sh
+++ nextcloud-duplicates-tagger.sh
@@ -11,6 +11,7 @@
 #
 ##############################################################################################
 # Searches for duplicate files in the specified user account and tags them
+VERSION="v2.0"
 ##############################################################################################
 
 # the tag name for duplicates
@@ -40,14 +41,21 @@ NextCloudPath=/var/www/nextcloud
 F_HELP(){
     cat <<_EOH
 
-    # LICENSE: GPL v3
-    # AS-IS without any warranty
-    #
-    # Copyright 2020-2021 Georgiy Sitnikov
-    # Copyright 2024 steadfasterX <steadfasterX | AT | gmail #DOT# com>
-    # https://github.com/steadfasterX/nextcloud_scripts/blob/main/nextcloud-duplicates-tagger.sh
-    #
+    Version: $VERSION
+
     # Searches for duplicate files in the specified user account and tags them
+    # AS-IS without any warranty
+
+    Requirements
+    ---------------------
+
+    You have to tag at least 1 file MANUALLY with these 2 tags BEFORE running this tool:
+
+        - {tagName}
+        - {tagName}_delme
+
+    {tagName} can be whatever you like and will be specified within $0 or via CLI (see next sections)
+
 
     Usage
     ---------------------
@@ -58,6 +66,7 @@ F_HELP(){
     or when you want to run this for multiple users and/or servers instead:
         $> $0 -u "username" -p "password" -s "nextcloud-url" [--path,--tag,--limit,...]
 
+
     Optional parameters
     ---------------------
 
@@ -65,20 +74,60 @@ F_HELP(){
     -p | --password <password>  the corresponding (app) password
     -s | --server <nc-url>      the nextcloud base URI (e.g. https://yourFQDN/nextcloud)
          --path <nc-path>       local path to your nextcloud installation
-    -t | --tag <tag name>       the tag name to be used (at least 1 file must have that tag manually set)
+    -t | --tag <tag name>       the tag name to be used
+                                NOTE: check the above "Requirements" section!
     -l | --limit <nc dir>       limit the search to a specific sub dir (e.g. "Smartphone/camerapics")
     -d | --debug                enables debug mode
     -q | --quiet                disables any output (errors will be still shown though)
     -r | --rescan               forces a full re-scan
     -h | -help | --help | help  this screen ;)
+    --help-tags                 help screen how to work with the tagged files
 
     For convenience you can set any of the above directly within $0
     e.g. you could specify global settings like "--path" in $0
-    and user + password and any of the other as parameters on the CLI.
+    and user + password and some or all of the other as parameters at the CLI.
 
     Note: CLI parameters overwrite any defaults set in $0
     
 _EOH
+}
+
+F_HELPTAGS(){
+    cat <<_EOI
+
+    Duplicate files have been tagged with:
+
+    first occurence:    ${tagName}
+    further occurences: ${tagName}_delme
+
+    What's next?
+
+    There are 2 possible ways to go on from here:
+
+    1) single delete (recommended)
+    Open Files > Tags > $tagName within the UI or via the awesome "Memories" app
+    You will see duplicates site by site so you can select those you want to delete
+
+    2) batch delete (at your own risk)
+    Open Files > Tags > ${tagName}_delme within the UI or via the awesome "Memories" app
+    All files here should be save to delete in a single run because all these should
+    have 1 file tagged as >${tagName}< which is not shown up in the >${tagName}_delme< listing.
+
+    !!BUT .... BEWARE OF THE DRAGONS!!
+
+    Of course nextcloud provides a trash bin but I would not rely on that alone.
+    Before deleting any files tagged with either >${tagName}< or >${tagName}_delme<
+    always do a backup before. You have been warned...
+
+    -------------------------------
+    In ANY case keep in mind:
+    all this is AT YOUR OWN RISK
+    -------------------------------
+
+    have fun ;)
+
+_EOI
+ 
 }
 
 ReScan=no
@@ -87,6 +136,7 @@ ReScan=no
 while [ ! -z "$1" ];do
     case $1 in
         help|--help|-help|-h) F_HELP; exit;;
+        --help-tags) echo -e "\n This output is dynamic,\n use: $0 --tag <tag name> --help-tags\n if not using the default tag name set in $0\n"; F_HELPTAGS; exit;;
         -u|--user) user="$2"; shift 2;;
         -p|--password) password="$2"; shift 2;;
         -s|--server) NextcloudURL="$2"; shift 2;;
@@ -137,7 +187,7 @@ getFileID () {
 	else
 
 		[[ "$LogLvL" == "Debug" ]] && { echo "[DEBUG] FileID is $fileid."; }
-		return
+		return 0
 
 	fi
 
@@ -173,7 +223,7 @@ getTag () {
 
 SetTag () {
 
-	[ $LogLvL != "None" ] && echo "[PROGRESS] Setting tag $tagName for $fileToTag."
+	[ $LogLvL == "Debug" ] && echo "[DEBUG] Setting tag $tagName for $fileToTag."
 	curl -s -m 10 -u $user:$password "$NextcloudURL/remote.php/dav/systemtags-relations/files/$fileid/$tagID" \
 	 -X 'PUT' \
 	 -H 'content-type: application/json' \
@@ -206,7 +256,7 @@ checkIfTagIsSet () {
 		else
 
 			[[ "$LogLvL" == "Debug" ]] && echo "[DEBUG] Tag $tagName is not set"
-			SetTag && [[ "$LogLvL" != "None" ]] && echo "[INFO] Tag $tagName has been set successfully"
+			SetTag && [[ "$LogLvL" != "None" ]] && echo "[PROGRESS] Setting tag $tagName for $fileToTag."
 
 		fi
 
@@ -281,19 +331,53 @@ fileToTagPath() {
 
 checkLockFile
 
+# save real tagname
+real_tagName="${tagName}"
+tagName="${tagName}_delme"
 getTag
+delme_tagID=$tagID
+tagName="${real_tagName}"
+getTag
+real_tagID=$tagID
 
 # Will use existing Tag report
 [[ -s "$LOCKFILE" ]] || { findDuplicates; }
 
+# loop over every line, parse, check tag and set if needed
 while read line; do
 
-	# reading each line
+    fileToTag=$(fileToTagPath)
 
-	fileToTag=$(fileToTagPath)
+    # get the file hash
+    chash=$(echo "$line" | cut -c -32)
 
-	getFileID && checkIfTagIsSet
+    # check if this is the first time we see this hash
+    # and set the "${tagName}_delme" tag if not
+    if [ "$chash" != "$phash" ];then
+        [ $LogLvL == "Debug" ] && echo "[DEBUG] processing the FIRST duplicate ($chash | $fileToTag)"
+    else
+        [ $LogLvL == "Debug" ] && echo "[DEBUG] processing ANOTHER duplicate ($chash | $fileToTag)"
+        tagName="${tagName}_delme"
+        tagID="$delme_tagID"
+        getFileID && checkIfTagIsSet
+    fi
+
+    # reset tagname
+    tagName="${real_tagName}"
+    tagID="$real_tagID"
+
+    # tag the duplicated file, this will tag the duplicated file(s) twice:
+    # once with ${tagName}_delme (see above) and also just with ${tagName} to make
+    # it easier identifying & deleting via the UI
+    getFileID && checkIfTagIsSet
+
+    # set phash to the current hash value
+    phash="$chash"
 
 done < $LOCKFILE
+ERR=$?
 
-[ $LogLvL != "None" ] && echo "Script ended with $?"
+if [ $LogLvL != "None" ];then
+    echo "[INFO] Script ended with $ERR"
+    F_HELPTAGS   
+fi
